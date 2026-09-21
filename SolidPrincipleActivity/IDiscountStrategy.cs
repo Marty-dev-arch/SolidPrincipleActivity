ec6d38d979eaae4828444e686c0dd50a9ce50992
Marty-dev-arch <l.goboy.145625.tc@umindanao.edu.ph>
2026-09-22 01:54:27 +0800

    Add project files.

---
+++ SolidPrincipleActivity/IDiscountStrategy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrincipleActivity
+{
+    public interface IDiscountStrategy
+    {
+        decimal Apply(decimal total);
+    }
+
+    public class NoDiscount : IDiscountStrategy
+    {
+        public decimal Apply(decimal total) => total;
+    }
+
+    public class StudentDiscount : IDiscountStrategy
+    {
+        public decimal Apply(decimal total) => total * 0.9m;
+    }
+
+    public class SeniorDiscount : IDiscountStrategy
+    {
+        public decimal Apply(decimal total) => total * 0.85m;
+    }
+
+    public class BlackFridayDiscount : IDiscountStrategy
+    {
+        public decimal Apply(decimal total) => total * 0.7m;
+    }
+
+    public class FreeShippingDiscount : IDiscountStrategy
+    {
+        public decimal Apply(decimal total)
+        {
+            throw new NotSupportedException("Doesn't apply to order totals, only shipping!");
+        }
+    }
+}
---
+++ SolidPrincipleActivity/IEmailService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace SolidPrincipleActivity
+{
+    public interface IEmailService
+    {
+        void SendInvoice(string recipientEmail, decimal total);
+    }
+
+    public interface IPrinterService
+    {
+        void PrintInvoice(string recipientEmail, decimal total);
+    }
+
+    public class SmtpEmailService : IEmailService
+    {
+        private readonly string _smtpHost;
+        private readonly string _gmailAccount;
+        private readonly string _appPassword;
+
+        public SmtpEmailService(
+            string smtpHost = "smtp.gmail.com",
+            string gmailAccount = "l.goboy.145625.tc@umindanao.edu.ph.test-google-a.com",
+            string appPassword = "xlkf nriw qyla pxgn")
+        {
+            _smtpHost = smtpHost;
+            _gmailAccount = gmailAccount;
+            _appPassword = appPassword;
+        }
+
+        public void SendInvoice(string recipientEmail, decimal total)
+        {
+            if (!MailAddress.TryCreate(recipientEmail?.Trim(), out var recipient))
+            {
+                throw new ArgumentException("Enter a valid recipient email address.", nameof(recipientEmail));
+            }
+
+            if (!MailAddress.TryCreate(_gmailAccount?.Trim(), out var sender))
+            {
+                throw new InvalidOperationException("The configured sender email address is invalid.");
+            }
+
+            using var smtp = new SmtpClient(_smtpHost)
+            {
+                Port = 465,
+                EnableSsl = true,
+                Credentials = new NetworkCredential(_gmailAccount, _appPassword)
+            };
+
+            using var mail = new MailMessage(sender, recipient)
+            {
+                Subject = "Invoice",
+                Body = $"Your total is {total:C}"
+            };
+
+            smtp.Send(mail);
+        }
+    }
+}
